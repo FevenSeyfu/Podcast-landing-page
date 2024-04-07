66e81c1add97659d47ea64698b176821573d561e
FevenSeyfu <fevenseyfu00@gmail.com>
2024-04-07 23:05:48 +0300

    uninstall tailwind and complete component structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,43 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      Pod Landing Page
+      <nav>
+        <img src="src/assets/Logo.png" alt="Pod Logo" />
+      </nav>
+      <main>
+        <section className="hero-text">
+          <h1 className=" ">
+            <span className="">Publish your podcasts</span> <br />{" "}
+            everywhere.
+          </h1>
+          <p>
+            Upload your audio to Pod with a single click. We’ll then distribute
+            your podcast to Spotify, Apple Podcasts, Google Podcasts, Pocket
+            Casts and more!
+          </p>
+          <div>
+            <form>
+              <input type="text"  placeholder="Email address"/>
+              <button> Request Access</button>
+            </form>
+            <ul>
+              <li><img src="src/assets/Spotify_logo_with_text.png" alt="" /></li>
+              <li><img src="src/assets/Apple_podcast.png" alt="" /></li>
+              <li><img src="src/assets/GooglePodcast.png" alt="" /></li>
+              <li><img src="src/assets/PocketCasts.png" alt="" /></li>
+            </ul>
+          </div>
+        </section>
+        <section className="hero-img">
+          <img src="src/assets/Bitmap.png" alt="" />
+        </section>
+        
+      </main>
+      <span className="combined-shapes"></span>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
